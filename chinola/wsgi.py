5d6d0a35f84bb90b35d7608e33d0e11fca559e5a
Ricardo Ramos <ricardoramos@Ricardo-Ramoss-MacBook-Pro.local>
2018-07-20 10:01:38 -0400

    Updated structure

--- chinola/wsgi.py
+++ chinola/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChC.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chinola.settings")
 
 application = get_wsgi_application()
