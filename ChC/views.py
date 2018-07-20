a87d8d4a42e3ebdeaf46cd2d5f761555dc835ea8
Ricardo Ramos <ricardoramos@Ricardo-Ramoss-MacBook-Pro.local>
2018-07-20 09:59:33 -0400

    Updated structure

--- ChC/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from . import templates
-
-def index(request):
-
-	return render(request, 'import_csv.html', {})
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ChC.wsgi --log-file -
+web: gunicorn chinola.wsgi 
